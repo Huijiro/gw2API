78da3e358bcd850a8c19d1c8ee8f0e411788dfc3
Huijiro <huijirohankei@gmail.com>
2024-01-17 15:53:31 -0300

    ci: setup semantic-release

---
+++ release.config.cjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    // Not yet pushing the package to npm.
+    // "@semantic-release/npm",
+  ]
+};
